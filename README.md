8b4138ab8ab4975019393448857ce2500a7ccd58
Qasim Zeeshan <qasim.zeeshan@coeus-solutions.de>
2015-06-26 15:27:32 +0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # php_bootcamp
+- Checkout this repo using 'git clone'
+- Install VirtualBox
+- Install Vagrant
+- Run 'vagrant up'
+
+Hit http://localhost:8080/bootcamp
---
+++ index.php
@@ -0,0 +1,9 @@
+<h1><?php
+echo 'Welcome to Coeus bootcamp';
+?></h1>
+
+<ul>
+	<li>Create a file hello_world.php in your cloned directory with some php code and hit http://localhost:8080/bootcamp/hello_world.php</li>
+	<li>You can play with mysql on <a href="http://localhost:8080/adminer" target="_blank">http://localhost:8080/adminer</a>. Credentials are root/123</li>
+   	<li>If you want to have some fun with SSH, open terminal, go to your repo directory and enter 'vagrant ssh'</li>
+</ul>
